8154a8335e194f9cb4613f01d94e75a0068d11b1
valmsmith39a <valmsmith39a@gmail.com>
2020-04-05 21:40:19 -0700

    environment variables, google news api, ex get company stock news

--- routes/company.js
+++ routes/company.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET company news and analysis */
 router.get('/news-analysis', function(req, res, next) {
-  res.send('company news and analysis');
+  var NEWS_API_KEY = process.env.NEWS_API_KEY
+  var BASE_URL = 'http://newsapi.org/v2'
+  var NEWS_SEARCH_TYPE = '/everything?'
+  var url = BASE_URL + NEWS_SEARCH_TYPE + 
+          'q=TSLA&' +  
+          'from=2020-04-01&' +
+          'sortBy=popularity&' +
+          'apiKey=' + 
+          NEWS_API_KEY
+
+  fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    res.send(data)
+  });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/company-news-analysis', function(req, res, next) {
-  res.json({test: 'hello test'})
-})
-
 module.exports = router;
